cb9a1ed7637e4ef72616079635569cd4c82c7232
Elliott Linder <elliott.darfink@gmail.com>
2017-05-24 12:22:04 +0200

    refactor(iOS): merge 'Main' and 'AppDelegate' classes

--- Platform/iOS/Main.cs
+++
@@ -1,9 +0,0 @@
-namespace Ainomis.Platform.Ios {
-  using UIKit;
-
-  public class Application {
-    internal static void Main(string[] args) {
-      UIApplication.Main(args, null, "AppDelegate");
-    }
-  }
-}
